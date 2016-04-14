552719bcc1b4c7ca26941e5873e4f77e33ce9421
丁一鸣 <315129552@qq.com>
2016-04-15 02:17:29 +0800

    更改成功,

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ AV.initialize(APP_ID, APP_KEY, MASTER_KEY);
 // 如果不希望使用 masterKey 权限，可以将下面一行删除
 AV.Cloud.useMasterKey();
 
-var app = require('./app');
+var app = require('./cloud/app');
 
 // 端口一定要从环境变量 `LC_APP_PORT` 中获取。
 // LeanEngine 运行时会分配端口并赋值到该变量。
 var PORT = parseInt(process.env.LC_APP_PORT || 3000);
 app.listen(PORT, function () {
-  console.log('Node app is running, port:', PORT);
+  console.log('端口号:', PORT);
 });
