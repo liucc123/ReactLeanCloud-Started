700645e4a027c8f412c4c8b7f5f0576fdee3bc17
丁一鸣 <315129552@qq.com>
2016-04-15 04:11:55 +0800

    初步添加react成功

---
+++ spa/src/index.js
@@ -0,0 +1,15 @@
+/*
+* @Author: dingyiming
+* @Date:   2016-04-07 15:28:02
+* @Last Modified by:   dingyiming
+* @Last Modified time: 2016-04-07 15:36:20
+*/
+
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+	<h1>基于react.js的一个小测试，又踩了个坑</h1>,document.getElementById('root')
+	);
