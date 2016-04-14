22d48b451fbf9d06dc2cd1b3224ba492934cf366
丁一鸣 <315129552@qq.com>
2016-04-15 02:44:46 +0800

    修复静态资源路径

--- cloud/app.js
+++ cloud/app.js
@@ -9,10 +9,12 @@ var cloud = require('./cloud');
 
 var app = express();
 
-// 设置 view 引擎
+// 设置 view 路径和引擎
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+
+//配置静态资源路径
+app.use(express.static('cloud/public'));
 
 // 加载云代码方法
 app.use(cloud);
@@ -52,6 +54,7 @@ app.get('/', function(req, res) {
 
 // 可以将一类的路由单独保存在一个文件中
 app.use('/todos', todos);
+
 
 // 如果任何路由都没匹配到，则认为 404
 // 生成一个异常让后面的 err handler 捕获
